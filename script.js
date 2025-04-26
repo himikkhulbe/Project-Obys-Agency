090855bc306b21d5158145f8c6cc7f4e01d17a8d
Himik Khulbe <himikkhulbe@gmail.com>
2025-04-26 13:55:16 +0530

    Made website responsive

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function lenisAnimation(){
       lenis.on('scroll', ({ scroll }) => {
         const fastDiv = document.querySelector('#page2')
         // Move it at faster scroll speed
-        fastDiv.style.transform = `translateY(${scroll * 0.1}px)`
+        fastDiv.style.transform = `translateY(${scroll * 0.13}px)`
       })
       
 }
@@ -80,9 +80,10 @@ tl.to(".line h2", {
 })
 
 tl.to("#loader",{
+    y: -1500,
     opacity: 0,
     duration: 0.5,
-    delay: 0 /*chane to 1.5*/
+    delay: 0 /*change to 1.5*/
 })
 
 tl.from("#page1",{
@@ -111,17 +112,172 @@ tl.from("#page2",{
 }
 
 function cursorAnimation(){
-    document.addEventListener("mousemove", function(dets){
-        gsap.to("#cursor",{
-            x: dets.x-20,
-            y: dets.y-20
+    // document.addEventListener("mousemove", function(dets){
+    //     gsap.to("#cursor",{
+    //         x: dets.x-20,
+    //         y: dets.y-20
+    //     })
+    // })
+
+    Shery.mouseFollower({
+        skew: true,
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 0.5,
+    });
+    
+    let videoContainer = document.querySelector("#video-container")
+    let video = document.querySelector("#video-container video")
+    videoContainer.addEventListener("mouseenter", function(){
+        videoContainer.addEventListener("mousemove", function(dets){
+            gsap.to(".mousefollower", {
+                opacity: 0
+            })
+            gsap.to("#video-cursor", {
+                x: dets.x-1250,
+                y: dets.y-350
+            })
+        })
+    })
+    videoContainer.addEventListener("mouseleave", function(){
+        gsap.to(".mousefollower",{
+            opacity: 1
+        })
+        gsap.to("#video-cursor", {
+            top: "-12%",
+            left: "70%"
         })
     })
 
-    Shery.makeMagnet("#nav-part2 h4");
+    let flag = 0
+    videoContainer.addEventListener("click",function(){
+        if(flag == 0){
+            video.play()
+            video.style.opacity = 1;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-large-line"></i>`
+            gsap.to("#video-cursor",{
+                scale: 0.5
+            })
+            flag = 1
+        } else {
+            video.pause()
+            video.style.opacity = 0;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-large-fill"></i>`
+            gsap.to("#video-cursor",{
+                scale: 1
+            })
+            flag = 0
+        }
+    })
+}
+
+function sheryAnimation(){
+    Shery.imageEffect(".image-div",{
+        style: 5,
+        config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272756554789667},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.52,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.31,"range":[0,10]},"metaball":{"value":0.4,"range":[0,2]},"discard_threshold":{"value":0.4,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.42,"range":[0,2]},"noise_scale":{"value":7.9,"range":[0,100]}}, 
+        gooey: true
+    })
+}
+
+function flagAnimation(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#flag",{
+            x: dets.x,
+            y: dets.y
+        })
+    })
+    document.querySelector("#hero3").addEventListener("mouseenter", function(){
+        gsap.to("#flag",{
+            opacity:1
+        })
+    })
+    document.querySelector("#hero3").addEventListener("mouseleave", function(){
+        gsap.to("#flag",{
+            opacity:0
+        })
+    })
+}
+
+function magnetEffect(){
+    // Shery.makeMagnet("#nav-part2 h4");
+    Shery.makeMagnet("#nav-part2 h4"); 
 }
 
 // locomotiveAnimation()
 lenisAnimation()
 loadingAnimation()
 cursorAnimation()
+sheryAnimation()
+flagAnimation()
+magnetEffect()
+
+let letsCreate = document.querySelector("#letsCreate")
+letsCreate.addEventListener("mouseenter", function(){
+    letsCreate.style.fontFamily = "silk serif"
+    letsCreate.style.webkitTextStroke = '1px #fff';
+    letsCreate.style.color = "transparent"
+    letsCreate.style.fontStyle = "italic";
+    letsCreate.style.fontWeight = "400"
+    letsCreate.style.paddingBottom = "1.4vw"
+    letsCreate.style.opacity = "1"
+    gsap.from("#letsCreate", {
+        opacity: 0,
+        duration: 0.3,
+        onStart: function(){
+            $('#footer h1').textillate({ in: { effect: 'fadeIn' } });
+        }
+    })
+})
+letsCreate.addEventListener("mouseleave", function(){
+    letsCreate.style.fontFamily = "Plain Light"
+    letsCreate.style.opacity = "1"
+    letsCreate.style.color = "#fff"
+    letsCreate.style.fontStyle = "normal";
+    letsCreate.style.fontWeight = "400"
+    letsCreate.style.paddingBottom = "4vw"
+    gsap.from("#letsCreate", {
+        opacity: 0,
+        duration: 0.5,
+        onStart: function(){
+            $('#footer h1').textillate({ in: { effect: 'fadeIn' } });
+        }
+    })
+})
+
+
+
+
+// function splitString(str){
+//     let splittedTextHtml='',generatedHTML='';
+//     let string = str.textContent;
+//     let i;
+//     for(i=0;i<string.length;i++){
+//       splittedTextHtml += `
+//       <span char="${string[i]}" style="--totalChars:${string.length};--index:${i};--delay:${i*100}ms;--duration:${string.length*100}ms">
+//       ${string[i]}
+//       </span>`;
+//     }
+    
+//     generatedHTML = `<div>${splittedTextHtml}</div>`
+//     str.innerHTML = generatedHTML;
+//   }
+  
+//   function splittingInit(){
+//     let splitCharArr = document.querySelectorAll('.split-text');
+//     splitCharArr.forEach((str)=>{
+//     splitString(str);
+//   })
+//   }
+
+//   let letsCreate = document.querySelector("#letsCreate")
+//   letsCreate.addEventListener("mouseenter", function(){
+//     gsap.from(".split-text span", {
+//         duration: 3,
+//         opacity: 0,
+//         y: 5,
+//         stagger: 0.1,
+//       });
+//   })
+  
+
+//   splittingInit();
+
