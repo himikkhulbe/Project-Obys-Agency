22d6aa915eafd157f4c72850a360f4360bdcd51f
Himik Khulbe <himikkhulbe@gmail.com>
2025-05-23 10:56:34 +0530

    Added all the animations & the project has now been completed

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// function locomotiveAnimation(){
-//     gsap.registerPlugin(ScrollTrigger);
-
-// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector("#main"),
-//   smooth: true
-// });
-// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-// locoScroll.on("scroll", ScrollTrigger.update);
-
-// // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-// ScrollTrigger.scrollerProxy("#main", {
-//   scrollTop(value) {
-//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-//   getBoundingClientRect() {
-//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-//   },
-//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-//   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-// });
-
-// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-// ScrollTrigger.refresh();
-// }
-
 function lenisAnimation(){
     const lenis = new Lenis({
         duration: 1.2,
@@ -83,7 +52,7 @@ tl.to("#loader",{
     y: -1500,
     opacity: 0,
     duration: 0.5,
-    delay: 0 /*change to 1.5*/
+    delay: 1.5 /*change to 1.5*/
 })
 
 tl.from("#page1",{
@@ -118,13 +87,7 @@ function cursorAnimation(){
     //         y: dets.y-20
     //     })
     // })
-
-    Shery.mouseFollower({
-        skew: true,
-        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-        duration: 0.5,
-    });
-    
+  
     let videoContainer = document.querySelector("#video-container")
     let video = document.querySelector("#video-container video")
     videoContainer.addEventListener("mouseenter", function(){
@@ -176,6 +139,14 @@ function sheryAnimation(){
         config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272756554789667},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.52,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.31,"range":[0,10]},"metaball":{"value":0.4,"range":[0,2]},"discard_threshold":{"value":0.4,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.42,"range":[0,2]},"noise_scale":{"value":7.9,"range":[0,100]}}, 
         gooey: true
     })
+
+    Shery.mouseFollower({
+        skew: true,
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 0.5,
+    });
+
+    Shery.makeMagnet("#nav-part2 h4")
 }
 
 function flagAnimation(){
@@ -197,87 +168,71 @@ function flagAnimation(){
     })
 }
 
-function magnetEffect(){
-    // Shery.makeMagnet("#nav-part2 h4");
-    Shery.makeMagnet("#nav-part2 h4"); 
+function letsCreateAnimation(){
+    let clutter = "";
+    let clutter2 = "";
+    
+    document.querySelector("#footer h1").textContent.split("").forEach(function(elem){
+        clutter += `<span>${elem}</span>`
+    })
+    document.querySelector("#footer h1").innerHTML = clutter;
+    
+    document.querySelector("#footer h2").textContent.split("").forEach(function(elem){
+        clutter2 += `<span>${elem}</span>`
+    })
+    document.querySelector("#footer h2").innerHTML = clutter2;
+    
+    
+    document.querySelector(".footer-text").addEventListener("mouseenter", function(){
+        gsap.to("#footer h1 span", {
+            opacity: 0,
+            stagger: 0.02
+        })
+        gsap.to("#footer h2 span", {
+            opacity: 1,
+            delay: 0.1,
+            stagger: 0.05
+        })
+    })
+    
+    document.querySelector(".footer-text").addEventListener("mouseleave", function(){
+        gsap.to("#footer h1 span", {
+            opacity: 1,
+            delay: 0.2,
+            stagger: 0.05
+        })
+        gsap.to("#footer h2 span", {
+            opacity: 0,
+            stagger: 0.02
+        })
+    })
 }
 
-// locomotiveAnimation()
+window.addEventListener("DOMContentLoaded", () => {
 lenisAnimation()
 loadingAnimation()
 cursorAnimation()
 sheryAnimation()
 flagAnimation()
-magnetEffect()
+letsCreateAnimation()
+});
 
-let letsCreate = document.querySelector("#letsCreate")
-letsCreate.addEventListener("mouseenter", function(){
-    letsCreate.style.fontFamily = "silk serif"
-    letsCreate.style.webkitTextStroke = '1px #fff';
-    letsCreate.style.color = "transparent"
-    letsCreate.style.fontStyle = "italic";
-    letsCreate.style.fontWeight = "400"
-    letsCreate.style.paddingBottom = "1.4vw"
-    letsCreate.style.opacity = "1"
-    gsap.from("#letsCreate", {
-        opacity: 0,
-        duration: 0.3,
-        onStart: function(){
-            $('#footer h1').textillate({ in: { effect: 'fadeIn' } });
-        }
-    })
-})
-letsCreate.addEventListener("mouseleave", function(){
-    letsCreate.style.fontFamily = "Plain Light"
-    letsCreate.style.opacity = "1"
-    letsCreate.style.color = "#fff"
-    letsCreate.style.fontStyle = "normal";
-    letsCreate.style.fontWeight = "400"
-    letsCreate.style.paddingBottom = "4vw"
-    gsap.from("#letsCreate", {
-        opacity: 0,
-        duration: 0.5,
-        onStart: function(){
-            $('#footer h1').textillate({ in: { effect: 'fadeIn' } });
-        }
-    })
-})
+// locomotiveAnimation()
+// lenisAnimation()
+// loadingAnimation()
+// cursorAnimation()
+// sheryAnimation()
+// flagAnimation()
+// letsCreateAnimation()
 
 
 
 
-// function splitString(str){
-//     let splittedTextHtml='',generatedHTML='';
-//     let string = str.textContent;
-//     let i;
-//     for(i=0;i<string.length;i++){
-//       splittedTextHtml += `
-//       <span char="${string[i]}" style="--totalChars:${string.length};--index:${i};--delay:${i*100}ms;--duration:${string.length*100}ms">
-//       ${string[i]}
-//       </span>`;
-//     }
-    
-//     generatedHTML = `<div>${splittedTextHtml}</div>`
-//     str.innerHTML = generatedHTML;
-//   }
-  
-//   function splittingInit(){
-//     let splitCharArr = document.querySelectorAll('.split-text');
-//     splitCharArr.forEach((str)=>{
-//     splitString(str);
-//   })
-//   }
 
-//   let letsCreate = document.querySelector("#letsCreate")
-//   letsCreate.addEventListener("mouseenter", function(){
-//     gsap.from(".split-text span", {
-//         duration: 3,
-//         opacity: 0,
-//         y: 5,
-//         stagger: 0.1,
-//       });
-//   })
-  
 
-//   splittingInit();
+
+
+
+
+
 
